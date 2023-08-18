78e189a2f3a30ff79084063c3942b432d8e1436f
ubuntu <me@gmail.com>
2023-08-18 15:58:39 +0900

    update

--- cleanup.sh
+++ cleanup.sh
@@ -1,3 +1,3 @@
 ps aux | grep ros | grep -v grep | awk '{ print "kill -9", $2 }' | sh
-g++ -o cleanup cleanup.cpp -lpigpiod_if2 -lrt
-sudo ./cleanup
+g++ -o /home/ubuntu/pendulum_project/pendulum_test/cleanup /home/ubuntu/pendulum_project/pendulum_test/cleanup.cpp -lpigpiod_if2 -lrt
+sudo /home/ubuntu/pendulum_project/pendulum_test/cleanup
